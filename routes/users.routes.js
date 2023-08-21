d989a983f1970a49dbb0d3873bcd78e38900c590
ze-e <white.noise616@gmail.com>
2023-08-20 20:10:27 -0600

    testing admin

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const verifyToken = require("../middleware/verifyToken");
+const verifyAdmin = require("../middleware/verifyAdmin");
 
 const {
   getAllUsers,
@@ -14,6 +15,7 @@ const {
 const router = express.Router();
 
 router.use(verifyToken);
+router.use(verifyAdmin);
 router.route("/users").get(getAllUsers);
 router.route("/login").post(loginUser);
 router.route("/register").post(createUser);
