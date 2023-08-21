bb52fb171a0d436066e68f8fa410414b0fa2c046
ze-e <white.noise616@gmail.com>
2023-08-20 19:35:59 -0600

    fix: verifyAdmin try catch

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -2,7 +2,13 @@ const User = require("../models/User.model");
 
 async function verifyAdmin(req, res, next) {
   //check if user is admin
-  const user = await User.findOne({ _id: req.user });
+  let user = null;
+  try {
+    user = await User.findOne({ _id: req.user });
+  }
+  catch (e) {
+    console.log("error verifying admin token");
+  }
   if(!user) req.isAdmin = false
   else req.isAdmin = user.admin.userType === "admin" ? true : false;
   next();
