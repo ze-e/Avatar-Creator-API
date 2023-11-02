aca5ee841302ee562323be991bbe565551ebb88f
ze-e <white.noise616@gmail.com>
2023-11-01 18:08:42 -0600

    fixed middleware

--- middleware/teacherRoute.js
+++ middleware/teacherRoute.js
@@ -1,10 +1,7 @@
-const User = require("../models/User.model");
-
 async function teacherRoute(req, res, next) {
     // check if user is teacher
-    if (req.role !== "teacher")
-      throw new Error(`Only teachers have access to this route`);
-    next();
+    if (req.role !== "teacher") res.status(500).json({ error: `Only teachers have access to this route`});
+    else next();
 }
 
 module.exports = teacherRoute;
